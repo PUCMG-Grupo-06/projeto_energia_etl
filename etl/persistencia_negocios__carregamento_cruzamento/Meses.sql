8ab7d7736b544e547103a16694e627a3e8aeb81b
Bruno Devesa <103903230+brunosdvs@users.noreply.github.com>
2022-06-01 09:56:54 -0300

    Atualizando etl Tarifas e toda a base de dados
    Tabela de Tarifa simplificada
    Adicionando tabela de meses
    Adicionando relacoes entre as tabelas

---
+++ etl/persistencia_negocios__carregamento_cruzamento/Meses.sql
@@ -0,0 +1,38 @@
+/* Deve ser o último algoritmo a ser executado depois de todos os outros SQL*/
+
+/* ATUALIZA TABELAS EXISTENTES PARA RECEBER A CHAVE EXTRANGEIRA DO MÊS*/
+ALTER TABLE disponibilidade RENAME COLUMN dat_referencia TO mes;
+
+ALTER TABLE carga_media RENAME COLUMN din_instante TO mes;
+
+DELETE FROM geracao WHERE ano < 2000;
+ALTER TABLE geracao DROP COLUMN ano;
+ALTER TABLE geracao DROP COLUMN mes;
+ALTER TABLE geracao RENAME COLUMN data_referencia TO mes;
+
+/* CRIA TABELA DE MESES*/
+DROP TABLE IF EXISTS Meses;
+CREATE TABLE Meses(
+	mes date PRIMARY KEY
+	);
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Meses.csv'
+INTO TABLE Meses
+LINES TERMINATED BY '\n'
+(@mes)
+SET Meses.mes = STR_TO_DATE(@mes, '%Y-%m-%d');
+
+/* CONECTA AS OUTRAS TABELAS */
+ALTER TABLE Tarifa
+ADD CONSTRAINT FK_Tarifa_Mes FOREIGN KEY (mes) REFERENCES Meses(mes);
+
+ALTER TABLE Consumo
+ADD CONSTRAINT FK_Consumo_Mes FOREIGN KEY (mes) REFERENCES Meses(mes);
+
+ALTER TABLE Disponibilidade
+ADD CONSTRAINT FK_Disp_Mes FOREIGN KEY (mes) REFERENCES Meses(mes);
+
+ALTER TABLE Carga_Media
+ADD CONSTRAINT FK_CargaMedia_Mes FOREIGN KEY (mes) REFERENCES Meses(mes);
+
+ALTER TABLE Geracao
+ADD CONSTRAINT FK_Geracao_Mes FOREIGN KEY (mes) REFERENCES Meses(mes);
--- etl/persistencia_negocios__carregamento_cruzamento/Tarifa.sql
+++ etl/persistencia_negocios__carregamento_cruzamento/Tarifa.sql
@@ -1,13 +1,24 @@
-SET @@global.sql_mode= ''; /* Primeiro executa essa linha, reinicia o MySQL, daí pode tentar executar tudo. */
-
-create database if not exists projeto_energia;
+SET @@global.sql_mode= '';
 
 ALTER DATABASE projeto_energia CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
 
-use projeto_energia;
-
 DROP TABLE IF EXISTS Tarifa;
+DROP TABLE IF EXISTS SubGrupoTarifario;
 
+/* CRIA TABELA SUBGRUPOS E CARREGA DADOS */
+CREATE TABLE SubGrupoTarifario(
+	codigo varchar(3) PRIMARY KEY,
+    descricao varchar (40)
+    );
+    
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/SubGrupo.csv'
+INTO TABLE SubGrupoTarifario
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(codigo, descricao);
+
+/* CRIA TABELA TARIFA E CARREGA DADOS NELA */
 CREATE TABLE Tarifa (
 	id int not null auto_increment,
 	mes	date,
@@ -15,29 +26,20 @@ CREATE TABLE Tarifa (
 	cnpjDistribuidora numeric(14,0),
 	dataInicioVigencia date,
 	dataFimVigencia date,
-	baseTarifaria varchar(40),
-	dscSubGrupo varchar(40),
-	dscModalidadeTarifaria varchar(40),
-	dscClasse varchar(40),
-	dscSubClasse varchar(100),
-	dscDetalhe varchar(40),
-	nomPostoTarifario varchar(40),
-	unidadeTerciaria varchar(40),
-	sigAgenteAcessante varchar(40),
+	SubGrupo varchar(3),
+	ModalidadeTarifaria varchar(40),
 	vlrTUSD numeric(10,2),
 	vlrTe numeric(10,2),
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    CONSTRAINT FK_Tarifa_SubGrupoTarifario FOREIGN KEY (SubGrupo) REFERENCES SubGrupoTarifario(codigo) 
     );
 
-LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Tarifas.csv'
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/Tarifa.csv' 
 INTO TABLE Tarifa
 FIELDS TERMINATED BY ';'
-ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id,@Mes,nomeDistribuidora,cnpjDistribuidora,@DataInicioVigencia,@DataFimVigencia,
-BaseTarifaria,DscSubGrupo,DscModalidadeTarifaria,DscClasse,DscSubClasse,
-DscDetalhe,NomPostoTarifario,UnidadeTerciaria,SigAgenteAcessante,VlrTUSD,VlrTE)
-SET mes = STR_TO_DATE(@mes, '%Y-%m-%d'),
+(id,@Mes,nomeDistribuidora,cnpjDistribuidora,@DataInicioVigencia,@DataFimVigencia,SubGrupo,ModalidadeTarifaria,VlrTUSD,VlrTE)
+SET Tarifa.mes = STR_TO_DATE(@mes, '%Y-%m-%d'),
 	dataInicioVigencia = STR_TO_DATE(@dataInicioVigencia, '%Y-%m-%d'),
 	dataFimVigencia = STR_TO_DATE(@dataFimVigencia, '%Y-%m-%d');
