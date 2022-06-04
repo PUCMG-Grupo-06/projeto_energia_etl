c50f2e4a292afad2f3e2c14f6692bf6f2049f50d
aberriel <anselmo.lira1@gmail.com>
2022-06-04 14:23:44 -0300

    Removendo scripts python duplicados na pasta

--- etl/base_de_dados__extracao_tratamento/geracao_distribuida/__init__ 2.py
+++
@@ -1,8 +0,0 @@
-from .modelo_empreendimentos_geracao import ModeloEmpreendimentosGeracao
-from .modelo_geracao_distribuida import ModeloGeracaoDistribuida
-
-
-__all__ = [
-    'ModeloEmpreendimentosGeracao',
-    'ModeloGeracaoDistribuida'
-]
