a4bdd310ff70cf0520ec48218b9861deadced7c8
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-06-05 21:20:07 -0300

    Adicionando a coluna fonte_energia
    Adicionando a coluna "fonte_energia" no schema.sql
    Trocando o csv de geração

--- etl/base_de_dados__extracao_tratamento/csv's/Schema.sql
+++ etl/base_de_dados__extracao_tratamento/csv's/Schema.sql
@@ -114,24 +114,26 @@ SET mes = STR_TO_DATE(@mes, '%Y-%m-%d');
 
 /* CRIA TABELA geração E CARREGA DADOS NELA ----------------------------------------------------------------------------- */
 
+
 CREATE TABLE `geracao` (
   `mes` DATE,
-  `uf` varchar(20),
-  `total_pago` int,
-  `total_recebido` int,
-  `total_energia_entregue` int,
-  `total_energia_recebida` int,
-  `total_capacidade` int,
-  `geracao_total_MWmed` int
+  `uf` varchar(2),
+  `fonte_energia` varchar(50),
+  `total_pago` double,
+  `total_recebido` double,
+  `total_energia_entregue` double,
+  `total_energia_recebida` double,
+  `total_capacidade` double,
+  `total_geracao_centro_gravidade` numeric(20,2)
   );
   
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\geracao.csv'
 INTO TABLE geracao
-FIELDS TERMINATED BY ','   
+FIELDS TERMINATED BY ';'   
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS 
-(@mes,uf,total_pago,total_recebido,total_energia_entregue,total_energia_recebida,total_capacidade,geracao_total_MWmed) 
+(@mes,uf,fonte_energia,total_pago,total_recebido,total_energia_entregue,total_energia_recebida,total_capacidade,total_geracao_centro_gravidade) 
 SET mes = STR_TO_DATE(@mes, '%Y-%m-%d');
 
 /* CRIA TABELA DE MESES -------------------------------------------------------------------------------------------------------*/
