407b472f4e225b30138011499a50079516b60c71
SandersonEV <102122962+SandersonEV@users.noreply.github.com>
2022-06-26 17:52:50 -0300

    Limpeza e organização
    1. Removi a pasta csv's e integrei a parte atualizada do código SQL referente ao consumo e a geração aos arquivos anteriores.
    2. Coloquei os CSV's da geração e consumo atuais de volta em suas pastas originais.
    3. Remoção do arquivo 6correcoes.sql pois todas as alterações foram realizadas no código que deu origem ao CSV

--- etl/persistencia_negocios__carregamento_cruzamento/3consumo.sql
+++ etl/persistencia_negocios__carregamento_cruzamento/3consumo.sql
@@ -3,19 +3,17 @@ USE puc_projeto;
 DROP TABLE IF EXISTS consumo;
 
 CREATE TABLE consumo (
-    mes DATE,
-    subsistema VARCHAR(40),
-    uf VARCHAR(2),
-    setor VARCHAR(45),
-    consumo DOUBLE,
-    consumidores INT
-)  ENGINE=INNODB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci
+    `uf` varchar(2),
+    `mes` date,
+    `consumo` numeric(14,6)
+) ENGINE=INNODB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci
 ;
 LOAD DATA INFILE 'C:\\ProgramData\\MySQL\\MySQL Server 8.0\\Uploads\\consumo.csv'
 INTO TABLE consumo
-FIELDS TERMINATED BY ','  
+FIELDS TERMINATED BY ','   
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS 
-(@mes,subsistema,uf,setor,consumo,consumidores) 
-SET Mes = STR_TO_DATE(@Mes, '%Y-%m-%d');
+(uf,@mes,consumo) 
+SET mes = STR_TO_DATE(@mes, '%Y-%m-%d')
+;
